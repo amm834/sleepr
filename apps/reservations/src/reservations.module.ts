5e0f7879d8d2bed761353e3116439f62a127f2a7
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-08 05:04:54 +0630

    refactor: remove config module from individual service

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,9 +4,17 @@ import { ReservationsService } from './reservations.service';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './repositories';
 import { ReservationDocument, ReservationSchema } from './models';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
     DatabaseModule,
     DatabaseModule.forFeature([
       {
