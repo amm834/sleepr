69c8ffef87e7c1cc4a953baf709ea6ee011ac5b7
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-08 02:23:07 +0630

    feat: add validation for create reservation dto

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,9 +1,22 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateReservationDto {
+  @Type(() => Date)
+  @IsDate()
+  @IsNotEmpty()
   startDate: Date;
 
+  @IsDate()
+  @IsNotEmpty()
+  @Type(() => Date)
   endDate: Date;
 
+  @IsNotEmpty()
+  @IsString()
   placeId: string;
 
+  @IsNotEmpty()
+  @IsString()
   invoiceId: string;
 }
